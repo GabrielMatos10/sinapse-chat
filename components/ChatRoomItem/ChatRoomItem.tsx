cbcf488d5aa952390af5078a7159a935141da392
GabrielMatos10 <gabriel_de_pm@hotmail.com>
2022-03-09 17:17:55 -0300

    home screen

---
+++ components/ChatRoomItem/ChatRoomItem.tsx
@@ -0,0 +1,35 @@
+import { Text, View, Image, StyleSheet } from "react-native";
+import styles from './styles'
+import React from "react";
+
+
+export default function ({chatRoom}) {
+    const user = chatRoom.users[1]
+
+	return (
+		<View style={styles.container}>
+			<Image
+				style={styles.image}
+				source={{
+					uri: user.imageUri
+				}}
+			/>
+
+			{chatRoom.newMessages && <View style={styles.badgeContainer}>
+				<Text style={styles.badgeText}>{chatRoom.newMessages}</Text>
+			</View> }
+
+			<View style={styles.rightContainer}>
+				<View style={styles.row}>
+					<Text style={styles.name}>{user.name}</Text>
+					<Text style={styles.text}>{chatRoom.lastMessage.createdAt}</Text>
+				</View>
+				<Text numberOfLines={1} style={styles.text}>
+					{chatRoom.lastMessage.content}
+				</Text>
+			</View>
+		</View>
+	);
+}
+
+
